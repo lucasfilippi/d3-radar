7491f15e71b988c867425718cb2f8011d5e0efa9
Lucas Filippi <loucas@gmail.com>
2013-06-20 17:01:24 +0200

    Refactor plugin and improve examples

--- src/d3-radar.js
+++ src/d3-radar.js
@@ -79,23 +79,7 @@ function radar(metricConfiguration) {
             .attr('transform', 'rotate(' + (d.angle * 180 / Math.PI) + ')')
             .call(d3.svg.axis().scale(d.scale));
 
-          d3.select(this).append('text')
-            .text(d.label)
-            .attr('class', 'axisLabel')
-            .attr('x', function (d) {
-
-              var pos = labelOffset * Math.cos(d.angle);
-
-              if (Math.PI / 2 == Math.abs(d.angle)) {
-                pos -= this.getComputedTextLength() / 2;
-              } else if (Math.abs(d.angle) > Math.PI / 2) {
-                pos -= this.getComputedTextLength() ;
-              }
-
-              return pos;
-            })
-            .attr('y', labelOffset * Math.sin(d.angle));
-
+          chart.drawAxisLabel(d3.select(this));
         });
 
       // Add actual data
@@ -106,30 +90,7 @@ function radar(metricConfiguration) {
         .attr('class', function (d, i) { return 'radarArea radar radar' + i; })
         .each(function () {
 
-          d3.select(this)
-            .append('path')
-            .attr('class', 'radarPath')
-            .attr('d', function (d) {
-
-              return d3.svg.line()
-                .x(function (d) { return d.x; })
-                .y(function (d) { return d.y; })
-                .interpolate('linear-closed')
-                .call(this, d.points);
-            });
-
-          d3.select(this)
-            .selectAll('.radarPoint')
-            .data(function (d) {
-              return d.points;
-            })
-            .enter()
-            .append('circle')
-            .attr('cx', function (d) { return d.x; })
-            .attr('cy', function (d) { return d.y; })
-            .attr('r', pointRadius)
-            .attr('class', 'radarPoint')
-            .attr('id', function (d) { return 'radarPoint' + d.id; });
+          chart.drawArea(d3.select(this));
         });
 
     });
@@ -171,6 +132,51 @@ function radar(metricConfiguration) {
       .attr('cy', 0)
       .attr('class', 'background')
       .attr('r', function (d) { return d; });
+  };
+
+  chart.drawAxisLabel = function (selection) {
+
+    selection.append('text')
+      .text(function (d) { return d.label; })
+      .attr('class', 'axisLabel')
+      .attr('x', function (d) {
+
+        var pos = labelOffset * Math.cos(d.angle);
+
+        if (Math.PI / 2 === Math.abs(d.angle)) {
+          pos -= this.getComputedTextLength() / 2;
+        } else if (Math.abs(d.angle) > Math.PI / 2) {
+          pos -= this.getComputedTextLength();
+        }
+
+        return pos;
+      })
+      .attr('y', function (d) { return labelOffset * Math.sin(d.angle); });
+  };
+
+  chart.drawArea = function (selection) {
+
+    selection.append('path')
+      .attr('class', 'radarPath')
+      .attr('d', function (d) {
+        return d3.svg.line()
+          .x(function (d) { return d.x; })
+          .y(function (d) { return d.y; })
+          .interpolate('linear-closed')
+          .call(this, d.points);
+      });
+
+    selection.selectAll('.radarPoint')
+      .data(function (d) {
+        return d.points;
+      })
+      .enter()
+      .append('circle')
+      .attr('cx', function (d) { return d.x; })
+      .attr('cy', function (d) { return d.y; })
+      .attr('r', pointRadius)
+      .attr('class', 'radarPoint')
+      .attr('id', function (d) { return 'radarPoint' + d.id; });
   };
 
   return chart;
